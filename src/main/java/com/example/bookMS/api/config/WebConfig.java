0999daa81bafd91c31d480d5321999aef2be4a4f
nsg716 <1029cjswo@naver.com>
2025-12-09 13:19:58 +0900

    CORS add commit

--- src/main/java/com/example/bookMS/api/config/WebConfig.java
+++ src/main/java/com/example/bookMS/api/config/WebConfig.java
@@ -3,13 +3,17 @@ package com.example.bookMS.api.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://kt-aivle-school-8-4th-frontend-project-btg6fwyyh.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
